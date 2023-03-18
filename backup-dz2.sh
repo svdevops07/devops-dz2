bf73cd30574ec2edabd4cac816a8ea173e9200e5
Sergio <svdevops07@gmail.com>
2023-03-18 13:37:56 +0300

    Update path for copy

--- backup-dz2.sh
+++ backup-dz2.sh
@@ -3,4 +3,4 @@
 # generate non null files and send these to 2nd VM and safe 7 days
 
 base64 /dev/urandom | head -c 100000 > /tmp/files{1..3} 
-rsync -av --progress /tmp/files{1..3} root@devops2:/tmp/files{1..3} | find -type f -mtime +7 exec -rf {} \; 
+rsync -av --progress /tmp/files{1..3} root@devops2:/tmp/ | find -type f -mtime +7 exec -rf {} \; 
